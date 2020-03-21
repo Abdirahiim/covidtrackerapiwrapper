a5ef7c44fd2f7aecbe6d41a4ce218cbcf4fa3b9d
Abdirahiim Yassin <Abdirahiim1000@gmail.com>
2020-03-21 21:14:28 +0100

    Changed the name

--- CovidSharp Example/CovidSharp Example/Program.cs
+++ CovidSharp Example/CovidSharp Example/Program.cs
@@ -1,19 +1,23 @@
-# CovidSharp
-CovidSharp is a C# API wrapper for the Coronavirus tracking API(https://github.com/ExpDev07/coronavirus-tracker-api)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CovidSharp;
 
-## Example
-```c#
-static void Main(string[] args)
+namespace Coronavirus_tracker_API_wrapper_example
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-            // Declares the instance of the API
             CoronavirusData data = new CoronavirusData();
-            
-            // Fetches the latest data
+
             Console.WriteLine("Confirmed: " + data.LatestConfirmed());
             Console.WriteLine("Recovered: " + data.LatestRecovered());
             Console.WriteLine("Deaths: " + data.LatestDeaths());
 
-            // Fetches the latest data of a country
+            
             Console.WriteLine("Denmark Confirmed: " + data.FromCountryConfirmed("DK"));
             Console.WriteLine("Denmark Recovered: " + data.FromCountryRecovered("DK"));
             Console.WriteLine("Denmark Deaths: " + data.FromCountryDeaths("DK"));
@@ -22,13 +26,3 @@ static void Main(string[] args)
         }
     }
 }
-```
-## Prerequisites
-RestSharp
-Newtonsoft.json
-
-## Usage
-Add the library and the prerequisites as references
-
-## Installation
-Will soon be available as a nuget package
